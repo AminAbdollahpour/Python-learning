0941aed6ae28f564a293f4072cee02f72f682e19
Amin <108630429+AminAbdollahpour@users.noreply.github.com>
2022-07-10 23:35:09 +0430

    math functions

---
+++ nums.py
@@ -0,0 +1,9 @@
+num_first = -47.5
+print(round(num_first))
+print(abs(num_first))
+print(pow(8, 11))
+print(min(8, 11))
+from math import *
+print(floor(3.9))
+print(ceil(3.1))
+print(sqrt(169))
---
+++ strings.py
@@ -0,0 +1,3 @@
+character_name = "clark"
+character_age = 48
+print("i was firend with "+ character_name + " when she was "+str(character_age))
