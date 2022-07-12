4f81809d00cefc80537932b1aa50f32fd5883e3f
Amin <108630429+AminAbdollahpour@users.noreply.github.com>
2022-07-13 00:38:21 +0430

    if

--- if.py
+++ if.py
@@ -2,10 +2,10 @@ you_are_boy = True
 you_are_gamer = False
 
 if you_are_boy and you_are_gamer:
-    print("You better get a life bro")
+    print("You are a gamer boy")
 elif you_are_boy and not(you_are_gamer):
-    print("what do you do for fun?")
+    print("You are not a gamer boy")
 elif not(you_are_boy) and not(you_are_gamer):
-    print("That feels normal")
+    print("You are gamer a gamer girl")
 elif not(you_are_boy) and you_are_gamer:
-    print("You better get a life")
+    print("You are not a gamer girl")
