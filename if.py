8195a9ec2dee58512eed25fd16122e3fb1ca03d6
Amin <108630429+AminAbdollahpour@users.noreply.github.com>
2022-07-12 21:16:45 +0430

    if

---
+++ if.py
@@ -0,0 +1,11 @@
+you_are_boy = True
+you_are_gamer = False
+
+if you_are_boy and you_are_gamer:
+    print("You better get a life bro")
+elif you_are_boy and not(you_are_gamer):
+    print("what do you do for fun?")
+elif not(you_are_boy) and not(you_are_gamer):
+    print("That feels normal")
+elif not(you_are_boy) and you_are_gamer:
+    print("You better get a life")
