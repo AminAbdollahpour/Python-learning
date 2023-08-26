cd030f4cf2b665df582112ec99f8ff1b63b6265c
Amin Abdollahpour <mohammadaminabdolahpour@gamil.com>
2023-08-27 01:31:18 +0430

    committing old files

---
+++ Onject-oriented-programing/item.py
@@ -0,0 +1,71 @@
+import csv
+
+
+class Item:
+    pay_rate = 0.8
+    all = []
+
+    def __init__(self, name: str, price: float, quantity=0):
+        assert price >= 0, f"Price {price} is not geater than zero!"
+        assert quantity >= 0, f"Quantity is not greater than zero!0"
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception("The name is to long!")
+        else:
+            self.__name = value
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}',{self.price},{self.quantity})"
+
+    def __connect(self, smtp_server):
+        pass
+
+    def __prepare_body(self):
+        return f"""
+        anything
+        """
+
+    def __send(self):
+        pass
+
+    def __send_email(self):
+        self.__connect('')
+        self.__prepare_body()
+        self.__send()
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
