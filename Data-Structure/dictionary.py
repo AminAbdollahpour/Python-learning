281738ac83aaca61d996bbe4b57742f3abe5ea1d
asus <mohammadaminabdolahpour@gmail.com>
2024-01-29 19:40:58 +0330

    dic

---
+++ Data-Structure/dictionary.py
@@ -0,0 +1,20 @@
+me = {
+    'name': 'amin',
+    'lastname': 'abdollahpour',
+    'amin': 19,
+    'major': 'cs'
+}
+# nameExist = 'name' in me
+# ageValueExist = 19 in me.values()
+# me.clear()
+# me_copy = me.copy()
+# print(me_copy == me)
+# print(me_copy is me)
+
+
+# newUser = {'name': 'Unknown', 'family': 'Unknown'}
+# newUser2 = {}.fromkeys(['name','family'], 'Unknown')
+# print(newUser2)
+# print(me.get('gmail'))
+# major = me.pop('major')
+
